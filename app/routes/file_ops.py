b645b2b8876284bd9b44557ab364290b4c6c2a6f
veronica <v.escobar1@uniandes.edu.co>
2023-09-11 16:44:27 -0500

    [feat] Sucessfuly implemented function to upload project structure to azure blob storage.

--- app/routes/file_ops.py
+++ app/routes/file_ops.py
@@ -1,11 +1,20 @@
+import os
 import shutil
+import uuid
 import zipfile
 from pathlib import Path
 
-from fastapi import APIRouter, UploadFile
+from fastapi import FastAPI, Request, Form, UploadFile, File, Depends, APIRouter, HTTPException
+from azure.storage.blob import BlobServiceClient
 from starlette.responses import JSONResponse
 
 router = APIRouter()
+
+# TODO: Remove this and use an enviroment
+storage_account_key = 'UFMBntGTy48V7R4MuJfNOhi3zEeSnsvjrQhUCmjT3/Kz+08B8J0B5ghzC5nKfqg/vmZd+djElT88+AStnj0nAA=='
+storage_account_name = 'aiengineers'
+connection_string = 'DefaultEndpointsProtocol=https;AccountName=aiengineers;AccountKey=UFMBntGTy48V7R4MuJfNOhi3zEeSnsvjrQhUCmjT3/Kz+08B8J0B5ghzC5nKfqg/vmZd+djElT88+AStnj0nAA==;EndpointSuffix=core.windows.net'
+container_name = 'projects'
 
 
 @router.get("/")
@@ -14,7 +23,7 @@ async def test():
 
 
 @router.post("/upload")
-async def upload_directory(directory: UploadFile):
+async def upload_project_directory(directory: UploadFile):
     try:
         # Create temp dir to store the uploaded zip file containing the directory
         temp_dir = Path("temp_project_directory")
@@ -26,12 +35,32 @@ async def upload_directory(directory: UploadFile):
             shutil.copyfileobj(directory.file, buffer)
 
         # Extract directory content
-        target_directory = Path("target_directory")
         with zipfile.ZipFile(zip_file_path, "r") as zip_ref:
-            zip_ref.extractall(target_directory)
+            zip_ref.extractall(temp_dir)
 
-        # Upload project structure to azure storage service
+        # Upload project structure to azure storage service (recursive upload)
+        project_id_container = str(uuid.uuid4())
+
+        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+
+        recursive_project_upload(temp_dir, blob_service_client, project_id_container)
+
+        # Clean up: Remove the uploaded ZIP file
+        os.remove(zip_file_path)
 
         return JSONResponse(content={"message": "Directory uploaded successfully"}, status_code=200)
     except Exception as e:
         return JSONResponse(content={"messge": "Error uploading directory " + str(e)}, status_code=500)
+
+
+def recursive_project_upload(project_directory_path, blob_service_client, project_id):
+    for item in project_directory_path.iterdir():
+        if item.is_dir():
+            # Recursively upload subdirectories
+            recursive_project_upload(item, blob_service_client, project_id)
+        elif item.is_file():
+            # Upload files to Azure Blob Storage while preserving directory structure
+            blob_name = f"{project_id}/{item.relative_to(project_directory_path)}"
+            blob_client = blob_service_client.get_blob_client(container=container_name, blob=str(blob_name))
+            with open(item, "rb") as data:
+                blob_client.upload_blob(data, overwrite=True)
